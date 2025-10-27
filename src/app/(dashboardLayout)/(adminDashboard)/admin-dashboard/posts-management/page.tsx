9aae4665c3fbdb3b2f828488debd9db32c420048
Mehedi <mehedihasan67705251@gmail.com>
2025-10-27 19:31:37 +0600

    add margin-top to ManagePosts container for improved layout spacing

--- src/app/(dashboardLayout)/(adminDashboard)/admin-dashboard/posts-management/page.tsx
+++ src/app/(dashboardLayout)/(adminDashboard)/admin-dashboard/posts-management/page.tsx
@@ -3,7 +3,7 @@ import PostTable from "./_components/PostTable";
 
 export default async function ManagePosts() {
   return (
-    <div className="container mx-auto p-6 bg-default-50  rounded-lg">
+    <div className="container mx-auto p-6 bg-default-50  rounded-lg mt-8">
       <h1 className="text-2xl mb-6 border-l-5 border-primary font-bold pl-5">
         Manage Posts
       </h1>
